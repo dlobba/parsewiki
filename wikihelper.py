39710b77efbd65c87471429126cdbc45681af4b9
dlobba <diego.lobba@studenti.unitn.it>
2018-03-13 14:45:27 +0100

    added help message

--- wikihelper.py
+++ wikihelper.py
@@ -2,12 +2,17 @@ import parsewiki.utils as pwu
 import sys
 
 if __name__ == "__main__":
-    if sys.argv[2] == "-s":
-        page = pwu.parse_single_wikipage(sys.argv[1])
-        print(page.to_json())
-    elif sys.argv[2] == "-c":
-        for wikipage in pwu.bzip2_page_iter(sys.argv[1]):
-            for title, timestamp, page in pwu.iter_revisions(wikipage):
-                print(wikipage_to_json(page, title, timestamp))
-    elif sys.argv[2] == "-d":
-        pwu.split_bzip2(sys.argv[1], 5, 20, sys.argv[3])
+    try:
+        if sys.argv[2] == "-s":
+            page = pwu.parse_single_wikipage(sys.argv[1])
+            print(page.to_json())
+        elif sys.argv[2] == "-c":
+            for wikipage in pwu.bzip2_page_iter(sys.argv[1]):
+                for title, timestamp, page in pwu.iter_revisions(wikipage):
+                    print(wikipage_to_json(page, title, timestamp))
+        elif sys.argv[2] == "-d":
+            pwu.split_bzip2(sys.argv[1], 5, 20, sys.argv[3])
+    except IndexError:
+        print("Usage:\n\tparse single wikipage -s")
+        print("\tparse compressed bzip2 -c")
+        print("\tdivide bzip into chunks -d")
