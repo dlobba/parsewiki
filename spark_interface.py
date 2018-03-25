acfaa7ca8bd79edbe6e0547f608d13850deb6c52
dlobba <diego.lobba@studenti.unitn.it>
2018-03-25 14:58:34 +0200

    modified get_chunk and extracted function for online downloading

--- spark_interface.py
+++ spark_interface.py
@@ -1,20 +1,26 @@
+import sys
+import json
+import hashlib
+import logging
+
+import requests
 import findspark
-findspark.init("/home/gabriel/Scrivania/UNI/Master/17-18/BigDataSocialNetwork/spark-2.2.0-bin-hadoop2.7")
+import parsewiki.utils as pwu
+
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType
-import parsewiki.utils as pwu
-import requests
-import json
-import hashlib
-import sys
+
+class MD5IntegrityException(Exception): pass
+
+#findspark.init("/home/gabriel/Scrivania/UNI/Master/17-18/BigDataSocialNetwork/spark-2.2.0-bin-hadoop2.7")
 
 # TODO: search for what those True values are meant for
 schema = StructType([\
                      StructField("title", StringType(), True),\
                      StructField("link", StringType(), True)])
 
-def get_wikipedia_chunk(bzip2_file, max_numpage=20, max_iteration=None):
+def get_wikipedia_chunk(bzip2_source, max_numpage=20, max_iteration=None):
     """Return a bzip2 file containing wikipedia pages in chunks
     of a given number of pages.
 
@@ -23,14 +29,21 @@ def get_wikipedia_chunk(bzip2_file, max_numpage=20, max_iteration=None):
     then all the bzip2 file is processed.
 
     Params:
-      bzip2_file (str): path to the bzip2 file
+      bzip2_source: source to the bzip2 file, either a filename or
+        a bytearray
       max_numpage (int): length of each chunk
       max_iteration (int): number of iterations of the process
     """
     num_iteration = 0
     num_page = 0
     pages = []
-    for wikipage in pwu.bz2_memory_page_iter(bzip2_file):
+    # the method used to iterate over pages
+    # ALERT: pointer to function here!
+    page_iterator = pwu.bzip2_memory_page_iter
+    # if a filename is given then process as a bz2 file
+    if type(bzip2_source) == str:
+        page_iterator = pwu.bzip2_page_iter
+    for wikipage in page_iterator(bzip2_source):
         for revision in pwu.iter_revisions(wikipage):
             num_page += 1
             pages.append(revision)
@@ -64,6 +77,69 @@ def collect_links(json_page):
         results.append((title, un_sp[1]))
     return [result for result in results if result[1] is not None]
 
+def get_online_dump(wiki_version, max_dump=None, memory=False):
+    """Download a specific wikipedia version dumps online,
+    returing each dump.
+
+    Parameters:
+      wiki_version (str): date string of the wiki version to
+        work on
+      max_dump (int): the number of dump to take into account
+        (None if all dumps shuld be considered)
+      memory (bool): True if the dumps must be processed in memory.
+        If False then each dump is first saved temporarly into
+        the system and then processed as a file.
+
+    Notes:
+      Keep in mind that by working with dumps in memory, in case
+      the dump is 1GB then at least 1GB of ram is needed and then
+      a much higher requirement is needed whene decompression
+      is performed.
+    """
+    endpoint = "http://wikimedia.bytemark.co.uk"
+    status_url = endpoint + "/wikidatawiki/" + wiki_version + "/dumpstatus.json"
+    status_json = requests.get(status_url).json()
+    # testing dataset (no revision)
+    dumps = status_json['jobs']['articlesdump']
+    # full dataset (70TB)
+    #dumps = status_json['jobs']['metahistory7zdump']
+    if dumps['status'] != "done":
+        print("The dump is incomplete, stopping the execution...\n")
+        exit(-1)
+    files = []
+    for key in dumps['files'].keys():
+        files.append({'url': dumps['files'][key]['url'],\
+                      'md5': dumps['files'][key]['md5'],\
+                      'size': dumps['files'][key]['size']})
+    # sort in order to download smaller files first
+    files.sort(key=lambda file: file['size'])
+    # if not set, consider all files
+    if max_dump is None:
+        max_dump = len(files)
+
+    # filename used in case memory is set to False,
+    # notice that the filename is the same for all the dumps.
+    # So only the last dump is kept.
+    temp_filename = "/tmp/temp_dump.bz2" 
+    #remove [:1] to downoad whole wiki
+    for dump in files[:max_dump]:
+        url = endpoint + dump['url']
+        print(url)
+        bz2_dump = requests.get(url).content
+        # check dump integrity
+        if hashlib.md5(bz2_dump).hexdigest() != dump['md5']:
+            raise MD5IntegrityException("The dump download has a wrong MD5 value.")
+        logging.info("Correctly downloaded dump: {}".format(dump['url']))
+        
+        # with open("wikidatawiki-20171103-pages-articles19.xml-p19072452p19140743.bz2", 'rb') as dump:
+        #     bz2_dump = dump.read()
+        if memory is False:
+            with open(temp_filename, "wb") as bz2_fh:
+                bz2_fh.write(bz2_dump)
+            yield temp_filename
+        else:
+            yield bz2_dump
+
 
 if __name__ == "__main__":
     """Divide the wikipedia source into chunks of several
@@ -77,54 +153,52 @@ if __name__ == "__main__":
                         .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/test.coll") \
                         .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/test.coll") \
                         .getOrCreate()
-
-    # conf = SparkConf()
-    # conf.setMaster("local")
-    # conf.setAppName(")
-    # conf.set("spark.executor.memory", "1g")
-    # sc = SparkContext(conf=conf)
     sc = spark.sparkContext
-    pair_rdd = spark.createDataFrame(sc.emptyRDD(), schema).rdd
+    pairs_rdd = spark.createDataFrame(sc.emptyRDD(), schema).rdd
     json_text_rdd = spark.createDataFrame(sc.emptyRDD(), schema).rdd
+    pair_rdd = spark.createDataFrame(sc.emptyRDD(), schema).rdd
+    """
+    for chunk in get_wikipedia_chunk(sys.argv[1], max_numpage=20, max_iteration=3):
+        rdd = sc.parallelize(chunk)
+        json_text_rdd = rdd.map(jsonify)
+        # Note: here json_text_rdd contains
+        # one line of json for each wikipage
+        # so it's not a problem either if we
+        # would like to work on it as a single
+        # line or if we want to parse is as a
+        # single line json string (single line json
+        # and multiline json are treated differently
+        # by spark)
+
+        # json_df = spark.read.json(json_text_rdd)
+        # z = json_df.select('title').rdd.collect()
+        # for i in z:
+        #     print(i)
+
+        # try to read each line individually
+        current_pair_rdd = json_text_rdd.flatMap(collect_links)
+
+        # TODO: be sure that by doing this it would still work
+        # on clusters, since we are augmenting an rdd by
+        # itself...
+        pairs_rdd = sc.union([pairs_rdd, current_pair_rdd])
+
+    pairs_csv = pairs_rdd.map(lambda title_link_pair: str.join(";", title_link_pair))
+    pairs_csv.saveAsTextFile("../out")
+    """
 
     wiki_version = "20180201"
-    status_url = "http://wikimedia.bytemark.co.uk/wikidatawiki/"+wiki_version+"/dumpstatus.json"
-    status_json = requests.get(status_url).json()
-    dumps = status_json['jobs']['articlesdump'] # testing dataset (no revision)
-    #dumps = status_json['jobs']['metahistory7zdump'] # full dataset (70TB)
-    if dumps['status'] != "done":
-        print("Dump not ready\n")
-        exit(-1)
-    files = []
-    for key in dumps['files'].keys():
-        files.append({'url': dumps['files'][key]['url'], 'md5': dumps['files'][key]['md5'], 'size': dumps['files'][key]['size']})
-    files.sort(key=lambda file: file['size'])
-
-    for dump in files[:2]: #remove [:1] to downoad whole wiki
-        url = "http://wikimedia.bytemark.co.uk"+dump['url']
-        bz2_dump = requests.get(url).content
-        if hashlib.md5(bz2_dump).hexdigest() != dump['md5']: #fix it using haslib
-            break #TODO: MANAGE ERRORS
-        # with open("wikidatawiki-20171103-pages-articles19.xml-p19072452p19140743.bz2", 'rb') as dump:
-        #     bz2_dump = dump.read()
-        for chunk in get_wikipedia_chunk(bz2_dump, max_numpage=20, max_iteration=10):
+    # in this way each dump is first stored into
+    # a temporary file, set False to process it in memory
+    for dump in get_online_dump(wiki_version, True, 2):
+        print("dump downloaded, currently processing....")
+        for chunk in get_wikipedia_chunk(temp_filename, max_numpage=20, max_iteration=1):
             rdd = sc.parallelize(chunk)
             #rdd.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").save()
             json_text_rdd = rdd.map(jsonify)
             #json_text_rdd = json_text_rdd.union(rdd.map(jsonify))
-            # Note: here json_text_rdd contains
-            # one line of json for each wikipage
-            # so it's not a problem either if we
-            # would like to work on it as a single
-            # line or if we want to parse is as a
-            # single line json string (single line json
-            # and multiline json are treated differently
-            # by spark)
-
-            # json_df = spark.read.json(json_text_rdd)
-            # z = json_df.select('title').rdd.collect()
-            # for i in z:
-            #     print(i)
+            for i in json_text_rdd.collect():
+                print(i)
 
             # try to read each line individually
             pair_rdd = pair_rdd.union(json_text_rdd.flatMap(collect_links))
