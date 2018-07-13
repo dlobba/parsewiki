ac5345a8dc366a443e9d696b4eea0c9e93a7aa0f
dlobba <diego.lobba@studenti.unitn.it>
2018-07-13 12:14:37 +0200

    restructured task into separate functions (task1)

--- spark_interface.py
+++ spark_interface.py
@@ -21,28 +21,111 @@ from pyspark.sql.types import StructType, StructField, StringType
 
 logging.getLogger().setLevel(logging.INFO)
 
+## functions for task1 ---------------------------
 
+def jsonify(wiki_tuple):
+    title, timestamp, contributor, page = wiki_tuple
+    return pwu.wikipage_to_json(page, title, timestamp, contributor)
+
+def collect_heading(json_page):
+    """Return a tuple (page_title, timestamp, json_page)
+    for the given page."""
+    results = []
+    page = json.loads(json_page)
+    title = page['title']
+    timestamp = page['timestamp']
+    return title, timestamp, json_page
+
+def dump_to_json(spark_context, dump_iter, json_dir):
+    """Given a **function** to retrieve dumps (either
+    online or offline) and a folder where to store
+    results, transform dumps into json and store
+    them into the given folder.
+
+    Args:
+      spark_context: the context related to the task.
+      dump_iter(function): the function used to retrieve dumps.
+      json_dir(str): the folder used to store final .json files.
+    """
+    # TASK1: get the dump, max_numpage pages at a time.
+    # Transform to JSON the pages and create a .json
+    # file for each page, containing JSON objects representing
+    # page revisions.
+    for dump in dump_iter():
+        for chunk in pwu.get_wikipedia_chunk(dump,\
+                                             max_numpage=5,\
+                                             max_iteration=4):
+            chunk_rdd = spark_context.parallelize(chunk, numSlices=20)
+            json_text_rdd = chunk_rdd.map(jsonify)
+            json_heading_rdd = json_text_rdd\
+                               .map(collect_heading)
+
+            # now we want to store all revision of each
+            # page into a separate json file
+            # in json_dir
+            #
+            # first of all, obtain all pages so we can create
+            # a filename
+            page_titles = json_heading_rdd\
+                          .map(lambda entry: entry[0])\
+                          .distinct()\
+                          .collect()
+
+            json_pages = json_heading_rdd\
+                         .map(lambda entry: ((entry[0], entry[1]), entry[2]))\
+                         .sortByKey()\
+                         .collect()
+
+            # create (flushing) a new file for each page
+            filename_assoc = {title:0 for title in page_titles}
+            for page_title in page_titles:
+                filename = str.join("_", [word\
+                                       for word in re.split("\W",\
+                                                            page_title.lower())\
+                                       if word != ""])
+                filename_assoc[page_title] = filename
+                with open(json_dir + "{}.json".format(filename), "w") as fh:
+                    pass
+
+            for page in json_pages:
+                page_title = page[0][0]
+                filename = filename_assoc[page_title]
+                with open(json_dir + "{}.json".format(filename), "a") as fh:
+                    fh.write(str(page[1]) + "\n")
+
+            # push pages to mongoDB alternatively
+            #try:
+            #    links = spark.createDataFrame(pair_rdd, ["page", "link"])
+            #    links.write.\
+            #        format("com.mongodb.spark.sql.DefaultSource")\
+            #        .mode("append")\
+            #        .save()
+            #except ValueError:
+            #    logging.warning("Empty RDD.")
+
+# end functions for task1 ------------------------
+
+# functions for task2 ----------------------------
 def arg_sort(iterable):
     """Return an array with the indexes that
     would make the given array sorted following
     the ascending order."""
     return sorted(range(len(iterable)), key=lambda x: iterable[x])
 
-# If True then it's nullable
-schema = StructType([\
-                     StructField("title", StringType(), True),\
-                     StructField("link", StringType(), True)])
-
-
-def jsonify(wiki_tuple):
-    title, timestamp, contributor, page = wiki_tuple
-    return pwu.wikipage_to_json(page, title, timestamp, contributor)
-
 def collect_diff_elements(json_page):
     """Return a tuple (page, timestamp, text, location)
     for each token in the unstrucuted part.
     Text in case of links will contain the explicit link
-    and not the visible text appearing on the page."""
+    and not the visible text appearing on the page.
+
+    Note:
+      Due to the fact that normal text and liks are grouped
+      in the same way, we cannot distinguish one from
+      the other in any way.
+
+      A solution could be to prefix links with a special
+      mark.
+    """
     results = []
     page = json.loads(json_page)
     title = page['title']
@@ -56,14 +139,79 @@ def collect_diff_elements(json_page):
             results.append((title, timestamp, un_sp[0], un_sp[2]))
     return results
 
-def collect_heading(json_page):
-    """Return a tuple (page_title, timestamp, json_page)
-    for the given page."""
-    results = []
-    page = json.loads(json_page)
-    title = page['title']
-    timestamp = page['timestamp']
-    return title, timestamp, json_page
+def indexer(timestamp_list):
+    """Given a timestamp_list, sort it in ascending
+    order and associate to each element an increasing
+    index starting from 0.
+
+    Note:
+      It's possible to directly use the sort function
+      since revision timestamps follow the same string
+      format. If this wasn't the case, this decision
+      would have lead to problems.
+    """
+    ts_list = list(timestamp_list)
+    ts_list.sort()
+    result = []
+    for index in range(0, len(ts_list)):
+        result.append((ts_list[index], index))
+    return result
+
+def gen_pair_indexer(ts_index_pairs):
+    """Given an entry containing a list
+    of (timestampString, timestampIndex) pairs
+    return a list pairing consecutive timestamps:
+    
+        (<timestamp1Index, timestamp2Index>),
+            (timestamp1String, timestamp2String)
+
+    This allows to associate the timestamp of a
+    revision (either its string representation
+    and the numeric index associate) to the
+    string and numeric representation of the
+    timestamp of the next adjacent revision.
+
+    Note:
+      The use of the indexer function prior
+      to this is **assumed**.
+
+      Having associated the index we can establish
+      clearly the relationship between two
+      revision, meaning whichi one happened before
+      and whether they are adjacent with respect
+      to time.
+    """
+    tss_str = []
+    tss_index = []
+    for ts_str, ts_index in ts_index_pairs:
+        tss_str.append(ts_str)
+        tss_index.append(ts_index)
+        
+    result = []
+    for ind in range(0, len(ts_index_pairs)-1):
+        result.append((tss_index[ind], tss_index[ind + 1],\
+                       tss_str[ind], tss_str[ind + 1]))
+    return result
+
+def diff(entry):
+    if entry[0][0] < entry[1][0]:
+        entry_current = entry[0]
+        entry_successive = entry[1]
+    else:
+        entry_current = entry[1]
+        entry_successive = entry[0]
+
+    version_current = entry_current[0]
+    sequence_current = entry_current[1]
+    version_successive = entry_successive[0]
+    sequence_successive = entry_successive[1]
+    # used tokendiff instead of lcsdiff
+    return version_current, version_successive,\
+        tuple(tokendiff.token_diff(sequence_current, sequence_successive))
+
+
+# end functions for task2 ------------------------
+
 
 def collect_links(json_page):
     """Return a tuple (page, timestamp, link)
@@ -119,6 +267,17 @@ def versions_diff(iterable):
     Return:
       A tuple
       <current_timestamp, next_timestamp, action_type, word>.
+
+    Note:
+      This method is not actually the one we used, due to
+      its dumbness into noticing diffs.
+      In case of a single addition, this method would notice
+      diffs in each token following the effective diff element.
+
+      eg: "I like Trento" <--> "I really really like Trento"
+         diff-set: really really like Trento
+      Which is very imprecise. Eventually we opted to use
+      the difflib module provided by python.
     """
     list_iter = list(iterable)
     timestamps = [value[0] for value in list_iter]
@@ -167,51 +326,13 @@ def time_diff(iterable):
         results.append((diff_title, items_added, items_removed))
     return results
 
-def indexer(timestamp_list):
-    ts_list = list(timestamp_list)
-    ts_list.sort()
-    result = []
-    for index in range(0, len(ts_list)):
-        result.append((ts_list[index], index))
-    return result
 
-def gen_pair_indexer(ts_index_pairs):
-    """Given an entry containing a list
-    of (timestampString, timestampIndex) pairs
-    return a list pairing consecutive timestamps:
-    
-        (<timestamp1Index, timestamp2Index>),
-            (timestamp1String, timestamp2String)
-    """
-    tss_str = []
-    tss_index = []
-    for ts_str, ts_index in ts_index_pairs:
-        tss_str.append(ts_str)
-        tss_index.append(ts_index)
-        
-    result = []
-    for ind in range(0, len(ts_index_pairs)-1):
-        result.append((tss_index[ind], tss_index[ind + 1],\
-                       tss_str[ind], tss_str[ind + 1]))
-    return result
-
-def diff(entry):
-    if entry[0][0] < entry[1][0]:
-        entry_current = entry[0]
-        entry_successive = entry[1]
-    else:
-        entry_current = entry[1]
-        entry_successive = entry[0]
-
-    version_current = entry_current[0]
-    sequence_current = entry_current[1]
-    version_successive = entry_successive[0]
-    sequence_successive = entry_successive[1]
-    # used tokendiff instead of lcsdiff
-    return version_current, version_successive,\
-        tuple(tokendiff.token_diff(sequence_current, sequence_successive))
             
-            
+# If True then it's nullable
+schema = StructType([\
+                     StructField("title", StringType(), True),\
+                     StructField("link", StringType(), True)])
+         
 if __name__ == "__main__":
     """Divide the wikipedia source into chunks of several
     pages, each chunk is processed within an RDD in order
@@ -245,68 +366,22 @@ if __name__ == "__main__":
     spark = cu.spark_builder(SparkSession.builder, spark_configs)
     sc = spark.sparkContext
 
+    # ???
     pairs_rdd = spark.createDataFrame(sc.emptyRDD(), schema).rdd
     json_text_rdd = spark.createDataFrame(sc.emptyRDD(), schema).rdd
     pair_rdd = spark.createDataFrame(sc.emptyRDD(), schema).rdd
 
     json_dir = "/tmp/json/"
-    for dump in get_dump():
-        for chunk in pwu.get_wikipedia_chunk(dump, max_numpage=5, max_iteration=4):
-            # TASK1: get the dump, max_numpage pages at a time.
-            # Transform to JSON the pages and create a .json
-            # file for each page, containing JSON objects representing
-            # page revisions.
+    if not os.path.exists(json_dir):
+        os.makedirs(json_dir)
 
-            if not os.path.exists(json_dir):
-                os.makedirs(json_dir)
-            
-            rdd = sc.parallelize(chunk, numSlices=20)
-            json_text_rdd = rdd.map(jsonify)
-
-            json_heading_rdd = json_text_rdd\
-                       .map(collect_heading)
-
-            # obtain all pages, so we can create a filename
-            page_titles = json_heading_rdd\
-                         .map(lambda entry: entry[0])\
-                         .distinct()\
-                         .collect()
-
-            json_pages = json_heading_rdd\
-                         .map(lambda entry: ((entry[0], entry[1]), entry[2]))\
-                         .sortByKey()\
-                         .collect()
-
-            # create (flushing) a new file for each page
-            filename_assoc = {title:0 for title in page_titles}
-            for page_title in page_titles:
-                filename = str.join("_", [word\
-                                       for word in re.split("\W",\
-                                                            page_title.lower())\
-                                       if word != ""])
-                filename_assoc[page_title] = filename
-                with open(json_dir + "{}.json".format(filename), "w") as fh:
-                    pass
-                
-            for page in json_pages:
-                page_title = page[0][0]
-                filename = filename_assoc[page_title]
-                with open(json_dir + "{}.json".format(filename), "a") as fh:
-                    fh.write(str(page[1]) + "\n")
-            # push pages to mongoDB alternatively
-            # END TASK1
-            
-
-            # rdd union
-            # json_text_rdd = json_text_rdd.union(rdd.map(jsonify))
-
-            #try:
-            #    links = spark.createDataFrame(pair_rdd, ["page", "link"])
-            #    links.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").save()
-            #except ValueError:
-            #    logging.warning("Empty RDD.")
-
+    # TASK1
+    dump_to_json(sc, get_dump, json_dir)
+    sys.exit()
+    
     # TASK2.1: using LCS-based diff
+
+    # read json created previously in json_dir
     json_files = [file_ for file_ in os.listdir(json_dir)\
                   if file_[-5:] == ".json"]
     
@@ -327,21 +402,24 @@ if __name__ == "__main__":
         # obtain <P, [(t1_str, t1_int), ..., (tn_str, t2_int)]>
         # where timestamps are sorted
         timestamp_indexer_rdd = unstruct_parts_rdd\
-                                .map(lambda entry: (entry[0], (entry[1],)))\
+                                .map(lambda entry: (entry[0],\
+                                                    (entry[1],)))\
                                 .distinct()\
                                 .reduceByKey(lambda t1_list, t2_list:\
-                                             tuple(list(t1_list) + list(t2_list)))\
-
+                                             tuple(list(t1_list) +\
+                                                   list(t2_list)))
         str2int_rdd = timestamp_indexer_rdd\
                       .flatMapValues(indexer)\
-                      .map(lambda entry: ((entry[0], entry[1][0]), entry[1][1]))
+                      .map(lambda entry: ((entry[0], entry[1][0]),\
+                                          entry[1][1]))
 
         # by joining the two rdds we obtain
         #     (<P, TimestampString>, ((text, location), TimestampIndex))
         # we want to replace TimestampString with TimestampIndex
         # that's what the last map does
         vectorized_rdd = unstruct_parts_rdd\
-                         .map(lambda entry: ((entry[0], entry[1]), (entry[2], entry[3])))\
+                         .map(lambda entry: ((entry[0], entry[1]),\
+                                             (entry[2], entry[3])))\
                          .join(str2int_rdd)\
                          .map(lambda entry: ((entry[0][0], entry[1][1]),\
                                              entry[1][0]))
@@ -364,26 +442,29 @@ if __name__ == "__main__":
                                 ((entry[0][0], entry[0][1] + 1),\
                                  (entry[0][1], entry[1])))
 
+        # TODO: chek it out
         consecutive_ts_rdd = ts_rdd\
                              .leftOuterJoin(ts_successive_rdd)\
                              .filter(lambda entry:\
-                                     entry[1][0] is not None and entry[1][1])
+                                     entry[1][0] is not None and\
+                                     entry[1][1] is not None)
 
         # obtain diff objects:
         # <P, timestampIndex1, timestampIndex2, diff-list>
         diff_object_rdd = consecutive_ts_rdd\
                           .mapValues(diff)\
-                          .map(lambda entry: ((entry[0][0], entry[1][0], entry[1][1]),\
-                                              entry[1][2]))
+                          .map(lambda entry:\
+                               ((entry[0][0], entry[1][0], entry[1][1]),\
+                                entry[1][2]))
 
         # SUBSTASK: reversing timestamp index ----
 
-        
         int2str_rdd = timestamp_indexer_rdd\
                       .mapValues(indexer)\
                       .flatMapValues(gen_pair_indexer)\
-                      .map(lambda entry: ((entry[0], entry[1][0], entry[1][1]),\
-                                          (entry[1][2], entry[1][3])))
+                      .map(lambda entry:\
+                           ((entry[0], entry[1][0], entry[1][1]),\
+                            (entry[1][2], entry[1][3])))
 
         final_diff_rdd = int2str_rdd\
               .join(diff_object_rdd)\
@@ -395,8 +476,8 @@ if __name__ == "__main__":
 
         # keep in mind we are assuming to work with only one page,
         # if this is not the case this file will contain more pages
-        # differences (altough the diff operation takes into account different
-        # pages)
+        # differences (although the diff operation can take into
+        # account different pages)
         filename = json_file[:-5]
         with open("/tmp/diff_{}.txt".format(filename), "w") as fh:
             for i in final_diff_rdd.collect():
