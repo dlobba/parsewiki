3cfaf3e6e1d23031376dc8559fbf3854d6e60c10
dlobba <diego.lobba@studenti.unitn.it>
2018-07-13 10:18:03 +0200

    moved dump management and chunk methods to separate file.

--- spark_interface.py
+++ spark_interface.py
@@ -5,11 +5,11 @@ import hashlib
 import logging
 import re
 
-import requests
 import parsewiki.lcs as lcsdiff
 import parsewiki.diff as tokendiff
-import parsewiki.utils as pwu
 import cli_utils as cu
+import parsewiki.utils as pwu
+import dumpmanager as dm
 
 from nltk.corpus import stopwords
 from collections import OrderedDict
@@ -18,10 +18,10 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType
 
-class MD5IntegrityException(Exception): pass
 
 logging.getLogger().setLevel(logging.INFO)
- 
+
+
 def arg_sort(iterable):
     """Return an array with the indexes that
     would make the given array sorted following
@@ -33,48 +33,6 @@ schema = StructType([\
                      StructField("title", StringType(), True),\
                      StructField("link", StringType(), True)])
 
-def get_wikipedia_chunk(bzip2_source, max_numpage=5, max_iteration=1):
-    """Return a bzip2 file containing wikipedia pages in chunks
-    of a given number of pages.
-
-    It's possibile to limit the parsing by indicating the number
-    of iteration for the process to be repeated. If this is `None`
-    then all the bzip2 file is processed.
-
-    Params:
-      bzip2_source: source to the bzip2 file, either a filename or
-        a bytearray
-      max_numpage (int): length of each chunk
-      max_iteration (int): number of iterations of the process
-    """
-    if max_numpage <= 0:
-        raise ValueError("Insufficient number of pages specified.")
-    if max_iteration is not None and max_iteration <= 0:
-        raise ValueError("Insufficient number of iterations specified.")
-    num_iteration = 0
-    num_page = 0
-    pages = []
-    # the method used to iterate over pages
-    # ALERT: pointer to function here!
-    page_iterator = pwu.bzip2_memory_page_iter
-    # if a filename is given then process as a bz2 file
-    if type(bzip2_source) == str:
-        page_iterator = pwu.bzip2_page_iter
-    for wikipage in page_iterator(bzip2_source):
-        num_page += 1
-        for revision in pwu.iter_revisions(wikipage):
-            pages.append(revision)
-            # remember that revision is a tuple
-            # (title, timestamp, contributor, plain_wikitext)
-            if num_page >= max_numpage:
-                yield pages
-                pages = []
-                num_page = 0
-                num_iteration += 1
-        if max_iteration and num_iteration >= max_iteration:
-            break
-    if len(pages) > 0:
-        yield pages
 
 def jsonify(wiki_tuple):
     title, timestamp, contributor, page = wiki_tuple
@@ -238,83 +196,7 @@ def encode_timestamp(entry):
     out = OrderedDict({key: 0 for key in page_ts})
     for change in change_ts:
         out[change] = 1
-    return (entry[0], (page_ts, list(out.values())))
-       
-def get_online_dump(wiki_version, max_dump=None, memory=False):
-    """Download a specific wikipedia version dumps online,
-    returing each dump.
-
-    Parameters:
-      wiki_version (str): date string of the wiki version to
-        work on
-      max_dump (int): the number of dump to take into account
-        (None if all dumps shuld be considered)
-      memory (bool): True if the dumps must be processed in memory.
-        If False then each dump is first saved temporarly into
-        the system and then processed as a file.
-
-    Notes:
-      Keep in mind that by working with dumps in memory, in case
-      the dump is 1GB then at least 1GB of ram is needed and then
-      a much higher requirement is needed whene decompression
-      is performed.
-    """
-    endpoint = "http://wikimedia.bytemark.co.uk"
-    status_url = endpoint + "/wikidatawiki/" + wiki_version + "/dumpstatus.json"
-    status_json = requests.get(status_url).json()
-    # testing dataset (no revision)
-    dumps = status_json['jobs']['articlesdump']
-    # full dataset (70TB)
-    #dumps = status_json['jobs']['metahistory7zdump']
-    if dumps['status'] != "done":
-        print("The dump is incomplete, stopping the execution...\n")
-        exit(-1)
-    files = []
-    for key in dumps['files'].keys():
-        files.append({'url': dumps['files'][key]['url'],\
-                      'md5': dumps['files'][key]['md5'],\
-                      'size': dumps['files'][key]['size']})
-    # sort in order to download smaller files first
-    files.sort(key=lambda file: file['size'])
-    # if not set, consider all files
-    if max_dump is None:
-        max_dump = len(files)
-
-    for dump in files[:max_dump]:
-        if not memory:
-            # Try to open the file matching with the md5sum of the resource.
-            # If it does not exist download it and write in the file as it's md5sum
-            filename_r = "/tmp/wikidump_" + dump['md5']
-            try:
-                with open(filename_r, "rt") as bz2_fh:
-                        # If the file exists we are sure that it is not corrupted.
-                        logging.info("Dump already downloaded, skipping download and reloading it.")
-                        yield filename_r
-            except FileNotFoundError:
-                url = endpoint + dump['url']
-                logging.info("Downloading dump: {}".format(url))
-                bz2_dump = requests.get(url).content
-                md5sum = hashlib.md5(bz2_dump).hexdigest()
-                if md5sum != dump['md5']:
-                    raise MD5IntegrityException("The dump downloaded has a wrong MD5 value.")
-                filename_w = "/tmp/wikidump_" + md5sum
-                with open(filename_w, "wb") as bz2_fh:
-                    bz2_fh.write(bz2_dump)
-                yield filename_w
-        else:
-            url = endpoint + dump['url']
-            logging.info("Downloading dump: {}".format(url))
-            bz2_dump = requests.get(url).content
-            yield bz2_dump
-
-def get_offline_dump(dump_folder):
-    """Given a path to the folder containing wikipedia
-    dumpps (in bz2 format), return the
-    path to each dump file"""
-    dump_files = [dump for dump in os.listdir(dump_folder) \
-                  if dump[-4:] == ".bz2"]
-    for dump in dump_files:
-        yield dump_folder + os.sep + dump
+    return (entry[0], (page_ts, list(out.values())))       
 
 def indexer(timestamp_list):
     ts_list = list(timestamp_list)
@@ -382,9 +264,9 @@ if __name__ == "__main__":
     # retrieve the dumps online
     if args.dump_dir:
         dump_dir = args.dump_dir
-        get_dump = lambda: get_offline_dump(dump_dir)
+        get_dump = lambda: dm.get_offline_dump(dump_dir)
     else:
-        get_dump = lambda: get_online_dump(wiki_version,\
+        get_dump = lambda: dm.get_online_dump(wiki_version,\
                                            max_dump,\
                                            process_in_memory)
     if args.spark_conffile:
@@ -400,7 +282,7 @@ if __name__ == "__main__":
 
     json_dir = "/tmp/json/"
     for dump in get_dump():
-        for chunk in get_wikipedia_chunk(dump, max_numpage=5, max_iteration=4):
+        for chunk in pwu.get_wikipedia_chunk(dump, max_numpage=5, max_iteration=4):
             # TASK1: get the dump, max_numpage pages at a time.
             # Transform to JSON the pages and create a .json
             # file for each page, containing JSON objects representing
